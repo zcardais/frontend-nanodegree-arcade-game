95b3d479c30822d7822c735be7f0ab794593e95c
Zach Cardais <zcardais@gmail.com>
2015-12-28 15:29:37 -0500

    The board, Player and Enemies all display on screen. Need to fix the following: 1) Enemy speeds are too slow, 2) Reset Player to starting position when it hits a bug, and 3) Reset Player to starting position when it reaches the water.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(startX,startY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = startX;
+    this.y = startY;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +21,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += (Math.random()*60*dt);
+    this.reset();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +30,69 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.reset = function() {
+  if (this.x >= 500) {
+    this.x = -101;
+    this.speed = getRandomArbitrary(250, 450);
+  }
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// Set Player starting position
+var playerStartX = 200,
+    playerStartY = 400;
+
+// Write the Player class
+var Player = function(x,y) {
+  this.x = playerStartX;
+  this.y = playerStartY;
+  this.sprite = 'images/char-boy.png';
+};
+
+// Update the Player's position
+// TODO: reset Player position when it hits a bug
+Player.prototype.update = function() {
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+  if (direction === 'left') {
+    this.x -= 100;
+  }
+  if (direction === 'up') {
+    this.y -= 90;
+  }
+  if (direction === 'right') {
+    this.x += 100;
+  }
+  if (direction === 'down') {
+    this.y += 90;
+  }
+};
+
+Player.prototype.reset = function() {
+  this.x = playerStartX;
+  this.y = playerStartY;
+}
 
 // Now instantiate your objects.
+var allEnemies = [];
+// Instantiate all enemies
+for (var i = 0; i < 3; i++) {
+  var tempSpeed = Math.floor(Math.random() * 5 + 1) * 75;
+  allEnemies.push(new Enemy(-60, 60 + 85 * i, tempSpeed));
+};
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+
+var player = new Player(playerStartX,playerStartY);
 
 
 // This listens for key presses and sends the keys to your
