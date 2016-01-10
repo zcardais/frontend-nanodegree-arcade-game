ac7aa2dfb17a5400548148a82485a742145dfdd0
Zach Cardais <zcardais@gmail.com>
2016-01-10 13:20:38 -0500

    Fixed randomizing enemy bug speeds. TODO: fix checkCollisions() code.

--- js/app.js
+++ js/app.js
@@ -13,12 +13,12 @@ var Enemy = function(startX,startY, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 Enemy.prototype.randomSpeed = function() {
   var speedMultiply = Math.floor(Math.random() * 5 + 1);
   this.speed = 75 * speedMultiply;
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -28,7 +28,7 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.speed * 1 * dt;
     this.reset();
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -36,16 +36,17 @@ Enemy.prototype.render = function() {
 };
 
 Enemy.prototype.reset = function() {
-  if (this.x >= 500) {
+  if (this.x > 500) {
     this.x = -60;
-    this.speed = getRandomArbitrary(500, 900); // This creates a new random speed for the enemy bug when after it resets. Without this, the speed of each enemy bug is the same for each "lap" it does. By adding this line of code, each bug starts a new lap at a random, and therefore different, speed.
+    // Create a new random speed for the enemy bug to use after it resets. Without this, the speed of each enemy bug is the same for each "lap." By adding this line of code, each bug begins a new lap at different speeds.
+    this.speed = getRandomArbitrary(500, 900);
   }
-}
+};
 
 Enemy.prototype.randomSpeed = function() {
   var speedMultiply = Math.floor(Math.random() * 5 + 1);
   this.speed = 75 * speedMultiply;
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -63,8 +64,15 @@ var Player = function(x,y) {
 }
 
 // Update the Player's position
-// TODO: reset Player position when it hits a bug
 Player.prototype.update = function() {
+  //collision detection
+  this.checkCollisions();
+
+  // Alert user they won if they reach the water and reset to starting position
+  if (this.y < 40 && this.y <= 400 && this.x >= 0 && this.x <= 400) {
+    alert("You win! Click 'OK' to play again.");
+    player.reset();
+  }
 }
 
 Player.prototype.render = function() {
@@ -76,32 +84,45 @@ Player.prototype.handleInput = function(direction) {
     this.x -= 100;
   }
   if (direction === 'up') {
-    this.y -= 90;
+    this.y -= 85;
   }
   if (direction === 'right') {
     this.x += 100;
   }
   if (direction === 'down') {
-    this.y += 90;
+    this.y += 85;
   }
-}
+  console.log(this.x, this.y);
+};
+
+Player.prototype.checkCollisions = function() {
+  for (var i = 0; i < allEnemies.length; i++) {
+    /*add calculation to determine row*/
+    if (this.x >= enemy.x + 0 &&
+        this.x < enemy.x + 100 &&
+        this.y >= enemy.y + 0 &&
+        this.y < enemy.y + 85) {
+          console.log('Splat!');
+        }
+    }
+};
 
 Player.prototype.reset = function() {
   this.x = playerStartX;
   this.y = playerStartY;
-}
+};
 
-// Now instantiate your objects.
-var allEnemies = [];
-// Instantiate all enemies
-for (var i = 0; i < 3; i++) {
-  var tempSpeed = Math.floor(Math.random() * 5 + 1) * 95;
-  allEnemies.push(new Enemy(-60, 60 + 85 * i, tempSpeed));
-}
-
+/*********** NOW INSTANTIATE ALL YOUR OBJECTS **********/
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate all enemies
+var allEnemies = [];
+for (var i = 0; i < 3; i++) {
+  var tempSpeed = Math.floor(Math.random() * 5 + 1) * 99;
+  enemy = new Enemy();
+  allEnemies.push(new Enemy(-100, (50 + (90 * i)), tempSpeed));
+}
 
+// Place the player object in a variable called player
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
@@ -113,6 +134,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     }
-
     player.handleInput(allowedKeys[e.keyCode]);
 })
